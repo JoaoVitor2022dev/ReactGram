687f76edca39cbc5ba0c293508d15b8b7a4e435c
joao vitor <githubconta2021@gmail.com>
2023-05-04 01:13:46 -0400

    Titulo: Config Do Banco De Dados / Criaçao dos models
    1 - configuramos com o mongodb atlas servidor web;
    2 - criamos o model Users;
    3 - criamos o model das fotos;

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,21 @@
 const mongoose = require("mongoose");
+const dbUser = process.env.BD_USER;
+const dbPassword = process.env.DB_PASSWORD; 
 
-// connection 
+const conn = async() => { 
+    try {
+        const dbConn = await mongoose.connect(
+        `mongodb+srv://${dbUser}:${dbPassword}@clusterreact.0ihcvim.mongodb.net/test`
+        ); 
+        console.log("Conectou ao banco!");
+
+        return dbConn;
+    } catch (err) {  
+        console.log(err);
+    }
+};
+
+conn();
+
+module.exports = conn;
+
