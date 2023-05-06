8225d54949f1b60df15934a366cdd1d17f86629f
joao vitor <githubconta2021@gmail.com>
2023-05-06 01:18:50 -0400

    Titulo: Criando Controllers e Config das Rotas
    1 - segurança com dados, colocando os dados da variavel do jwt no .env;
    2 - criando o jwt e usando o byscriptjs para descriptografar e ciptografa token;
    3 - configurando as rotas de usuario e normais

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,23 @@
+const User = require("../models/User");
+
+const bcrypt = require("bcryptjs"); 
+const jwt = require("jsonwebtoken");
+
+const jwtSecret = process.env.JWT_SECRET; 
+
+// Generate user Token 
+const GenerateToken = (id) => { 
+    return jwt.sign({id}, jwtSecret, {
+        expiresIn: "7d"
+    });
+};
+
+// register user and sing in 
+const register = async (req, res) => {
+    res.send("Registro")
+}
+
+module.exports = {
+    register
+}
+
