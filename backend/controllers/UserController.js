19b9326677708aff30181dba288956acd5608f1a
joao vitor <githubconta2021@gmail.com>
2023-05-11 00:56:58 -0400

    Titulo: verificaçao de token pela requisiçao, pegar toke por authenticaçao
    1 - 1 - valida se o token vem da requisiçao ;
    2 - validaçao se o token chega no sistema ;
    3 - ve se o token combina com nosso secret ;
    4 - capturar dados do usuario por meio do token pelo header ;
    5 - crir um middleaware de que ve se a verificaçao esta valida e acessivel...

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -73,11 +73,22 @@ const login = async (req, res) => {
         profileImage: user.profileImage,
         token: generateToken(user._id),
     }) 
-
 }; 
+
+
+//  get current logged in user 
+
+const getCurrentUser = async (req, res) => {  
+    
+  const user = req.user; 
+  
+  res.status(200).json(user);
+}; 
+
 
 module.exports = {
     register,
-    login
+    login,
+    getCurrentUser
 }
 
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,14 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 // controller 
-const { register, login } = require("../controllers/UserController");
+const { register, login, getCurrentUser } = require("../controllers/UserController");
 
-// middlewares 
+// middlewares para criar o methodo de aclopamento dos erros em array 
 const validate = require("../middlewares/handleValidation");
+
+// Middle de validaçoes de cada campo de dado
 const { userCreateValidation, loginValidation } = require("../middlewares/userValidations");
+
+// 
+const authGuard = require('../middlewares/authGuard');
 
 // Routes 
 router.post("/register", userCreateValidation(), validate, register);
 router.post("/login", loginValidation(), validate, login);
+router.get("/profile", authGuard, getCurrentUser );
+
 
 module.exports = router
