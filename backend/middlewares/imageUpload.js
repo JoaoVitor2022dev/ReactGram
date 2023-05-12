2e5478af300fc51b6a98c63f308492052709399a
joao vitor <githubconta2021@gmail.com>
2023-05-11 22:03:00 -0400

    Titulo: Middleware de Upload de Imagens
    1 - importar methodo multer e path ;
    2 - criar a destinaçao para as imagens ;
    3 - criar o nome para as imagens, exemple: (1324132673.jng)  ;
    4 - por fim, upload da imagen, com verificaçao sobre o tipo da imagem png jpg jpeg ;

---
+++ backend/middlewares/imageUpload.js
@@ -0,0 +1,36 @@
+const multer = require('multer'); 
+const path = require("path");
+
+// Destination to store image 
+const imageStore = multer.diskStorage({
+    destination: function (req, file, cb) {
+        let folder = ""
+
+        if (req.baseUrl.includes("users")) {
+            folder = "users"
+        } else if(req.baseUrl.includes("photos")){
+            folder = "photos"
+        }
+
+        // destino da imagen
+        cd(null, `uploads/${folder}/`);
+    },
+    fileName: (req, file, cb) => {
+      // mudar o nome do arquivo 
+      cb(null, Date.now() + path.extname(file.originalname))
+   }
+})
+
+const imageUpload = multer({
+    storage: imageStore,
+    fileFilter(req, file, cb){
+        if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
+             // upload only png and jpg formats 
+             
+             return cb(new Error("Por favor, apenas envie png ou jpeg ou jpg!"))
+        }
+        cb(undefined, true);
+    }
+})
+
+module.exports = {  imageUpload };
