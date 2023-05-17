d74b9617b21e7cc4d8a01b70d784b10d8092c256
joao vitor <githubconta2021@gmail.com>
2023-05-17 00:18:12 -0400

    Titulo: Resgatando Dados de Todas As Fotos
    1 - criando function no controller, resgatando todos as photos do banco de dados ;
    2 - criando rota de photo com authGuard.js ;

--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // controller 
-const { insertPhoto, deletePhoto } = require("../controllers/PhotoController");
+const { insertPhoto, deletePhoto, getAllPhotos } = require("../controllers/PhotoController");
 
 // middleware 
 const { photoInsertValidation } = require("../middlewares/photoValidations"); 
@@ -12,6 +12,7 @@ const { imageUpload } = require("../middlewares/imageUpload");
 
 // router
 router.post("/", authGuard, imageUpload.single("image"), photoInsertValidation(), validate, insertPhoto); 
-router.delete("/:id", authGuard, deletePhoto )
+router.delete("/:id", authGuard, deletePhoto ); 
+router.get("/", authGuard, getAllPhotos)
 
 module.exports = router; 
