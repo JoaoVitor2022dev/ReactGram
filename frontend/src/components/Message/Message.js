e17d3dd65a30064bbc1fa4b24d18d3c69ec175a2
joao vitor <githubconta2021@gmail.com>
2023-05-24 13:45:34 -0400

    Titulo: Componente de Message
    1 - criar componente para type e msg e com classname dinamixa por type ;
    2 - criar css para ambas msg de error e success ;

---
+++ frontend/src/components/Message/Message.js
@@ -0,0 +1,14 @@
+// styles 
+import './Message.css'
+
+import React from 'react'
+
+const Message = ({ msg, type}) => {
+  return (
+    <div className={`message ${type}`}>
+        {msg}
+    </div>
+  )
+}
+
+export default Message;
