235cf8249a0e0e5ca856fe1d3c33157acc59ad00
joao vitor <githubconta2021@gmail.com>
2023-05-26 14:38:42 -0400

    Titulo: HTML e CSS da pagina de EditProfile
    1 - desenvolvendo o html
    2 - criando o css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import { useAuth } from "./hooks/useAuth";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
+import EditProfile from "./pages/editProfile/EditProfile";
 
 // Componentes
 import Navbar from "./components/Navbar/Navbar";
@@ -30,9 +31,10 @@ function App() {
         <Navbar/>
          <div className="container">
             <Routes>
-              <Route path="/" element={auth ? <Home/>: <Navigate to="/login"/>}/>
-              <Route path="/login" element={!auth ? <Login/>: <Navigate to="/"/>}/>
-              <Route path="/register" element={!auth ? <Register/>: <Navigate to="/"/>}/>
+              <Route path="/" element={auth ? <Home/> : <Navigate to="/login"/>}/>
+              <Route  path="/profile" element={auth ? <EditProfile/> :<Navigate to="/login"/>} />
+              <Route path="/login" element={!auth ? <Login/> : <Navigate to="/"/>}/>
+              <Route path="/register" element={!auth ? <Register/> : <Navigate to="/"/>}/>
             </Routes>
          </div>
          <Footer/>
