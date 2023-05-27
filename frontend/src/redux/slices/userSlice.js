7a44034fed08ace3ef5a02d0ecf145aafcf8f502
joao vitor <githubconta2021@gmail.com>
2023-05-27 15:12:08 -0400

    Titulo: Slice de atualizaçao de perfil
    1 - slice finalizado e pronto para enviar states de message ;

--- frontend/src/redux/slices/userSlice.js
+++ frontend/src/redux/slices/userSlice.js
@@ -2,7 +2,6 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 
 import userService from "../services/userService";
 
-
 const initialState = {
     user: {},
     error: false,
@@ -19,6 +18,25 @@ export const profile = createAsyncThunk("user/profile", async (user, thunkAPI) =
 
     return data;
 });  
+
+// 1 - update user details
+
+export const updateProfile = createAsyncThunk("user/update", async (user, thunkAPI) => {
+     
+   const token = thunkAPI.getState.auth.user.token
+    
+   const data =  await userService.updateProfile(user, token);
+
+   // check for erros 
+
+   if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0])
+   }
+    
+   return data;
+}); 
+
+
 export const userSlice = createSlice({
     name: "user",
     initialState,
@@ -38,10 +56,27 @@ export const userSlice = createSlice({
            state.success = true;  
            state.error = null; 
            state.user = action.payload;
+       }).addCase(updateProfile.pending, (state) => { // loading
+        state.loading = true; 
+        state.error = false;
 
+       }).addCase(updateProfile.fulfilled, (state, action) => { // sucess
+        state.loading = false;  
+        state.success = true;  
+        state.error = null; 
+        state.user = action.payload;
+        state.message = "Usuario atualizado com sucesso!"
+
+       }).addCase(updateProfile.rejected, (state, action) => {  // error
+        state.loading = false; 
+        state.error = action.payload;
+        state.user = null;
        })
     }
 });
+
+
+
 
 export const { resetMessage } = userSlice.actions
 export default userSlice.reducer;
